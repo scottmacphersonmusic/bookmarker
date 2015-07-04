5a3a5de4f27c151e557b7aa0e2cc57815a975702
scott <scottmacphersonmusic@gmail.com>
2015-07-04 12:29:32 -0700

    Test index action

--- test/controllers/bookmarks_controller_test.rb
+++ test/controllers/bookmarks_controller_test.rb
@@ -1,6 +1,19 @@
 require 'test_helper'
 
 class BookmarksControllerTest < ActionController::TestCase
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:bookmarks)
+  end
+
+  test "index should render correct template and layout" do
+    get :index
+    assert_template :index
+    assert_template layout: "layouts/application"
+    assert_select "title", "Bookmarker"
+  end
+
   test "should not save without a link" do
     bookmark = Bookmark.new
     refute bookmark.save, "Saved the bookmark without a link"
