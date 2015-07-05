06afce4a9df4d8d7c269d2583f7f7d9de6d6353b
scott <scottmacphersonmusic@gmail.com>
2015-07-04 21:23:56 -0700

    Test edit and show action

--- test/controllers/bookmarks_controller_test.rb
+++ test/controllers/bookmarks_controller_test.rb
@@ -25,6 +25,12 @@ class BookmarksControllerTest < ActionController::TestCase
     assert_not_nil assigns(:bookmark)
   end
 
+  test "show should render correct template and layout" do
+    get :show, id: @bookmark
+    assert_template :show
+    assert_template layout: "layouts/application"
+  end
+
   test "should get new" do
     get :new
     assert_response :success
@@ -33,6 +39,18 @@ class BookmarksControllerTest < ActionController::TestCase
   test "new should render correct template and layout" do
     get :new
     assert_template :new
+    assert_template layout: 'layouts/application', partial: '_form'
+  end
+
+  test "should get edit" do
+    get :edit, id: @bookmark
+    assert_response :success
+    assert_not_nil assigns(:bookmark)
+  end
+
+  test "edit should render correct template and layout" do
+    get :edit, id: @bookmark
+    assert_template :edit
     assert_template layout: 'layouts/application', partial: '_form'
   end
 
